5d8b8f4a3f101777096d326b6439203d1f9d5686
Kareem Kamal <kareemkamal91@gmail.com>
2021-06-20 15:29:17 +0200

    fix(add): allow modal scroll and hide body overflow

--- src/app/home/components/AddModal.vue
+++ src/app/home/components/AddModal.vue
@@ -1,169 +1,169 @@
 <template>
   <div class="add-modal">
-    <button class="add-modal__backdrop" />
+    <div class="add-modal__container">
+      <div class="add-modal__body">
+        <div class="add-modal__fail" v-if="hasFailed">
+          <p class="add-modal__message">
+            Failed to add, please try again
+          </p>
 
-    <div class="add-modal__body">
-      <div class="add-modal__fail" v-if="hasFailed">
-        <p class="add-modal__message">
-          Failed to add, please try again
-        </p>
+          <button class="add-modal__button" @click="reset">Try again</button>
+        </div>
 
-        <button class="add-modal__button" @click="reset">Try again</button>
+        <div class="add-modal__success" v-else-if="hasSucceeded">
+          <p class="add-modal__message">
+            Success! your input will be visible once it's approved, you can update it at anytime using the same email address
+          </p>
+
+          <button class="add-modal__button" @click="$emit('cancel')">Close</button>
+        </div>
+
+        <form
+          v-else-if="step === 1"
+          @submit.prevent="onMandatoryDataSubmission"
+        >
+          <div class="add-modal__form-group">
+            <label for="email" class="add-modal__label">
+              Email*
+            </label>
+
+            <p class="add-modal__help-text">
+              الايميل هتقدر تستعمله ف انك تعدل البيانات
+            </p>
+
+            <input type="email" name="email" class="add-modal__input" required>
+          </div>
+
+          <div class="add-modal__form-group">
+            <label for="company" class="add-modal__label">
+              Company*
+            </label>
+
+            <p class="add-modal__help-text">
+              الشركة اللى سافرت ليها
+            </p>
+
+            <input type="text" name="company" class="add-modal__input" required>
+          </div>
+
+          <div class="add-modal__form-group">
+            <label for="travelPosition" class="add-modal__label">
+              Position*
+            </label>
+
+            <p class="add-modal__help-text">
+              المنصب اللى سافرت عليه
+            </p>
+
+            <input type="text" name="travelPosition" class="add-modal__input" required>
+          </div>
+
+          <div class="add-modal__form-group">
+            <label for="travelCountry" class="add-modal__label">
+              Travel country*
+            </label>
+
+            <input type="text" name="travelCountry" class="add-modal__input" required>
+          </div>
+
+          <div class="add-modal__form-group">
+            <label for="firstJobAbroadDetails" class="add-modal__label">
+              How I got my first job abroad*
+            </label>
+
+            <p class="add-modal__help-text">
+              تفاصيل التجربة
+            </p>
+
+            <textarea type="text" name="firstJobAbroadDetails" class="add-modal__input" required></textarea>
+          </div>
+
+          <div class="add-modal__controls">
+            <button
+              class="add-modal__button"
+              type="button"
+              @click="$emit('cancel')"
+            >
+              Cancel
+            </button>
+
+            <button class="add-modal__button">Next</button>
+          </div>
+        </form>
+
+        <form v-else-if="step === 2" @submit.prevent="onOptionalDataSubmission">
+          <h2 class="add-modal__title">Extra details (optional)</h2>
+          <div class="add-modal__form-group">
+            <label for="name" class="add-modal__label">
+              Name
+            </label>
+
+            <input type="text" name="name" class="add-modal__input">
+          </div>
+
+          <div class="add-modal__form-group">
+            <label for="linkedin" class="add-modal__label">
+              Linkedin
+            </label>
+
+            <input type="text" name="linkedin" class="add-modal__input">
+          </div>
+
+          <div class="add-modal__form-group">
+            <label for="source" class="add-modal__label">
+              Source
+            </label>
+            <p class="add-modal__help-text">
+              لينك للمصدر, فيديو او بوست على لينكد ان
+            </p>
+
+            <input type="text" name="source" class="add-modal__input">
+          </div>
+
+          <div class="add-modal__form-group">
+            <label for="travelDate" class="add-modal__label">
+              Travel date
+            </label>
+            <p class="add-modal__help-text">
+              سافرت امتى؟
+            </p>
+
+            <input type="date" name="travelDate" class="add-modal__input add-modal__date">
+          </div>
+
+          <div class="add-modal__form-group">
+            <label for="yearsOfExperienceAtTravel" class="add-modal__label">
+              Years of experience at the time of travel
+            </label>
+
+            <p class="add-modal__help-text">
+              كان عندك خبرة كام سنة وقت ما سافرت؟
+            </p>
+
+            <input type="number" name="yearsOfExperienceAtTravel" min="0" max="50" class="add-modal__input add-modal__number-input">
+          </div>
+
+          <div class="add-modal__form-group">
+            <label for="generalDetails" class="add-modal__label">
+              Other details
+            </label>
+
+            <textarea type="text" name="generalDetails" class="add-modal__input"></textarea>
+          </div>
+
+          <div class="add-modal__controls">
+            <button
+              class="add-modal__button"
+              type="button"
+              @click="$emit('cancel')"
+            >
+              Cancel
+            </button>
+
+            <button class="add-modal__button">Submit</button>
+          </div>
+        </form>
       </div>
-
-      <div class="add-modal__success" v-else-if="hasSucceeded">
-        <p class="add-modal__message">
-          Success! your input will be visible once it's approved, you can update it at anytime using the same email address
-        </p>
-
-        <button class="add-modal__button" @click="$emit('cancel')">Close</button>
-      </div>
-
-      <form
-        v-else-if="step === 1"
-        @submit.prevent="onMandatoryDataSubmission"
-      >
-        <div class="add-modal__form-group">
-          <label for="email" class="add-modal__label">
-            Email*
-          </label>
-
-          <p class="add-modal__help-text">
-            الايميل هتقدر تستعمله ف انك تعدل البيانات
-          </p>
-
-          <input type="email" name="email" class="add-modal__input" required>
-        </div>
-
-        <div class="add-modal__form-group">
-          <label for="company" class="add-modal__label">
-            Company*
-          </label>
-
-          <p class="add-modal__help-text">
-            الشركة اللى سافرت ليها
-          </p>
-
-          <input type="text" name="company" class="add-modal__input" required>
-        </div>
-
-        <div class="add-modal__form-group">
-          <label for="travelPosition" class="add-modal__label">
-            Position*
-          </label>
-
-          <p class="add-modal__help-text">
-            المنصب اللى سافرت عليه
-          </p>
-
-          <input type="text" name="travelPosition" class="add-modal__input" required>
-        </div>
-
-        <div class="add-modal__form-group">
-          <label for="travelCountry" class="add-modal__label">
-            Travel country*
-          </label>
-
-          <input type="text" name="travelCountry" class="add-modal__input" required>
-        </div>
-
-        <div class="add-modal__form-group">
-          <label for="firstJobAbroadDetails" class="add-modal__label">
-            How I got my first job abroad*
-          </label>
-
-          <p class="add-modal__help-text">
-            تفاصيل التجربة
-          </p>
-
-          <textarea type="text" name="firstJobAbroadDetails" class="add-modal__input" required></textarea>
-        </div>
-
-        <div class="add-modal__controls">
-          <button
-            class="add-modal__button"
-            type="button"
-            @click="$emit('cancel')"
-          >
-            Cancel
-          </button>
-
-          <button class="add-modal__button">Next</button>
-        </div>
-      </form>
-
-      <form v-else-if="step === 2" @submit.prevent="onOptionalDataSubmission">
-        <h2 class="add-modal__title">Extra details (optional)</h2>
-        <div class="add-modal__form-group">
-          <label for="name" class="add-modal__label">
-            Name
-          </label>
-
-          <input type="text" name="name" class="add-modal__input">
-        </div>
-
-        <div class="add-modal__form-group">
-          <label for="linkedin" class="add-modal__label">
-            Linkedin
-          </label>
-
-          <input type="text" name="linkedin" class="add-modal__input">
-        </div>
-
-        <div class="add-modal__form-group">
-          <label for="source" class="add-modal__label">
-            Source
-          </label>
-          <p class="add-modal__help-text">
-            لينك للمصدر, فيديو او بوست على لينكد ان
-          </p>
-
-          <input type="text" name="source" class="add-modal__input">
-        </div>
-
-        <div class="add-modal__form-group">
-          <label for="travelDate" class="add-modal__label">
-            Travel date
-          </label>
-          <p class="add-modal__help-text">
-            سافرت امتى؟
-          </p>
-
-          <input type="date" name="travelDate" class="add-modal__input add-modal__date">
-        </div>
-
-        <div class="add-modal__form-group">
-          <label for="yearsOfExperienceAtTravel" class="add-modal__label">
-            Years of experience at the time of travel
-          </label>
-
-          <p class="add-modal__help-text">
-            كان عندك خبرة كام سنة وقت ما سافرت؟
-          </p>
-
-          <input type="number" name="yearsOfExperienceAtTravel" min="0" max="50" class="add-modal__input add-modal__number-input">
-        </div>
-
-        <div class="add-modal__form-group">
-          <label for="generalDetails" class="add-modal__label">
-            Other details
-          </label>
-
-          <textarea type="text" name="generalDetails" class="add-modal__input"></textarea>
-        </div>
-
-        <div class="add-modal__controls">
-          <button
-            class="add-modal__button"
-            type="button"
-            @click="$emit('cancel')"
-          >
-            Cancel
-          </button>
-
-          <button class="add-modal__button">Submit</button>
-        </div>
-      </form>
     </div>
   </div>
 </template>
@@ -178,6 +178,14 @@ export default {
     hasFailed: false,
     hasSucceeded: false,
   }),
+
+  created() {
+    document.body.style.overflow = 'hidden'
+  },
+
+  beforeDestroy() {
+    document.body.style.overflow = ''
+  },
 
   methods: {
     onMandatoryDataSubmission(event) {
@@ -224,22 +232,20 @@ export default {
   position: fixed;
   width: 100%;
   height: 100%;
+  overflow: auto;
   top: 0;
   left: 0;
+  background-color: rgba(0,0,0,0.8);
+}
+
+.add-modal__container {
+  width: 100%;
+  min-height: 100%;
+  padding: 16px;
   display: flex;
   align-items: center;
   justify-content: center;
   padding: 16px;
-}
-
-.add-modal__backdrop {
-  background-color: rgba(0,0,0,0.8);
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  border: none;
 }
 
 .add-modal__body {
